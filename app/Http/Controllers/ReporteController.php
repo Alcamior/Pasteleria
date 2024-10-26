a3f59dc845f9103c62018dfac9ef3c229fb6a227
Camila Alor <alorcamila2@gmail.com>
2024-10-26 16:57:53 -0600

    Reporte de ventas diario terminado.

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -21,11 +21,20 @@ class ReporteController extends Controller
         $reporte = $request->input('formulario');
         $totalPP = [];
         $totalPC = [];
+        $total = [];
+        $ventas = [];
 
         switch ($reporte) {
             case 'formulario1':
+
                 $fecha = $request->input('fecha');
 
+                // Validación
+                $request->validate([
+                    'fecha' => 'required|date',
+                ]);
+
+                // Totales
                 $totalPP = DB::select('select sum(totalP) as totalPas from venta 
                 inner join pedido on venta.idv = pedido.idv
                 inner join producto on pedido.idpro = producto.idpro
@@ -39,6 +48,18 @@ class ReporteController extends Controller
                 and fechaVent = ? and pedido.status="Vendido";', [$fecha]);
 
                 $total = DB::select('select sum(total) as total from venta where fechaVent = ?;', [$fecha]);
+
+
+                // Información para tabla
+                $ventas = DB::select('select nombre, sum(cantidad) as cantidad, sum(totalP) as total
+                from venta inner join pedido on venta.idv = pedido.idv
+                inner join producto on pedido.idpro = producto.idpro
+                where fechaVent = ? and pedido.status="Vendido"
+                group by nombre;', [$fecha]);
+                
+                $fechaN = Carbon::createFromFormat('Y-m-d', $fecha)->format('Y - F - d');
+
+                return redirect()->route('reportes.ventas')->with(compact('totalPP', 'totalPC', 'total', 'reporte', 'fechaN', 'ventas'));
                 break;
 
             case 'formulario2':
@@ -46,9 +67,6 @@ class ReporteController extends Controller
 
             case 'formulario3':
                 break;
-        }
-
-        $fechaN = Carbon::createFromFormat('Y-m-d', $fecha)->format('Y - F - d');
-        return view('reportes.ventas', compact('totalPP', 'totalPC', 'total', 'reporte','fechaN'));
+        }        
     }
 }
